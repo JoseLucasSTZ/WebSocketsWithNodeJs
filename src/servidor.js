4f8787372eff48bf76d74d341c23518303f84b82
José Lucas Steinmetz da Silveira <65045856+joselucasSteinmetz@users.noreply.github.com>
2023-04-08 19:42:37 -0300

    update
    update

--- src/servidor.js
+++ src/servidor.js
@@ -1,7 +1,8 @@
 import express from "express";
 import url from "url";
 import path from "path";
-
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const porta = process.env.porta || 3000;
@@ -11,7 +12,14 @@ const diretorioPublico = path.join(caminhoAtual, "../..", "public");
 
 app.use(express.static(diretorioPublico));
 
+const servidorHttp = http.createServer(app);
 
-app.listen(porta, () => {
+servidorHttp.listen(porta, () => {
     console.log("Servidor escutando a porta " + porta)
+})
+
+const io = new Server(servidorHttp);
+
+io.on("connection", () => {
+    console.log("Um cliente se conectou!")
 })
